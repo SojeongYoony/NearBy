4002d8401c1cbef40a6880562798cb0e031db808
SoJeongYoon <y940425@gmail.com>
2021-12-29 02:46:43 +0900

    [2021-12-29 2:46am]

--- nearby/src/main/java/com/koreait/nearby/controller/ReplyController.java
+++ nearby/src/main/java/com/koreait/nearby/controller/ReplyController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.koreait.nearby.domain.Member;
 import com.koreait.nearby.domain.Reply;
 import com.koreait.nearby.service.ReplyService;
 
